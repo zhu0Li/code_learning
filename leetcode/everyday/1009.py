73e37ab71e0024fcd10a2111d47c359f2b68edac
zhu0Li <522192251@qq.com>
2022-10-09 15:23:49 +0800

    LeetCode学习，递归算法和括号的栈

---
+++ leetcode/everyday/1009.py
@@ -0,0 +1,37 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        st = [0]
+        for c in s:
+            if c == '(':
+                st.append(0)
+            else:
+                v = st.pop()
+                st[-1] += max(2 * v, 1)
+        return st[-1]
+
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        st = [0]
+        for c in s:
+            if c == '(':
+                st.append(0)
+            else:
+                v = st.pop()
+                st[-1] += max(2 * v, 1)
+
+        return st[-1]
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        ans = d = 0
+        for i, c in enumerate(s):
+            if c == '(':
+                d += 1
+            else:
+                d -= 1
+                if s[i - 1] == '(':
+                    ans += 1 << d
+        return ans
+if __name__ == '__main__':
+    x = "(((())))"
+    S = Solution()
+    print(S.scoreOfParentheses(x))
